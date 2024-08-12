08968ffe0fcb441d8c49719ad8b490dbba880d9b
Adesina Oluwaseun <oadesina@secureidltd.com>
2024-08-12 12:04:40 +0100

    [MODS]: Adding hover effects to Category section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,31 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'famous-undergarments';
   showMenu = false;
+  categorys: any[] = [
+    {
+      id: 1,
+      imageUrl: 'https://via.placeholder.com/600x800',
+      title: 'Boxers',
+      description: 'Comfortable and loose-fitting, perfect for everyday wear.',
+    },
+    {
+      id: 2,
+      imageUrl: 'https://via.placeholder.com/600x800',
+      title: 'Briefs',
+      description: 'Classic, snug fit for support and comfort.',
+    },
+    {
+      id: 3,
+      imageUrl: 'https://via.placeholder.com/600x800',
+      title: 'Boxer Briefs',
+      description: 'Combining the fit of briefs with the length of boxers.',
+    },
+  ];
+
+  activeCat: any;
 }
